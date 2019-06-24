eed842362b28540456b4c130da773b67e066677e
Andrew Carlson <acarl005@g.ucla.edu>
2019-06-23 21:23:05 -0500

    update license to ensure correctness

--- docsund-ui/src/components/D3Visualization/components/Inspector.jsx
+++ docsund-ui/src/components/D3Visualization/components/Inspector.jsx
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2002-2019 "Neo4j,"
+ * Modified work Copyright (c) 2019 Andrew Carlson
+ * Original work Copyright (c) 2002-2019 "Neo4j,"
  * Neo4j Sweden AB [http://neo4j.com]
  *
  * This file is part of Neo4j.
