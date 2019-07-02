656da7ba4b46684e6dae299bf63c6e319b23cfba
Danielle ONeil <danielle.oneil@berkeley.edu>
2019-07-02 16:09:34 -0500

    why didn't this get committed before

--- neuralcoref/spacy_processing.py
+++ neuralcoref/spacy_processing.py
@@ -8,18 +8,25 @@ import pandas as pd
 import multiprocessing as mp
 import numpy as np
 import time
+import re
 
 ENTITIES_OF_INTEREST = ['PERSON', 'NORP', 'FAC', 'ORG', 'GPE', 'LOC', 'PRODUCT', 'EVENT', 'WORK_OF_ART', 'LAW',
                         'LANGUAGE', 'DATE', 'TIME', 'PERCENT', 'MONEY', 'QUANTITY', 'ORDINAL', 'CARDINAL']
 NLP = spacy.load('en_core_web_sm')
 
-def load_data(data="emails_processed.csv", nrows=5000, skiprows=0):
-    base_df = pd.read_csv(data, nrows=nrows, skiprows=skiprows)
-    cols = ['Message-ID', 'To', 'From', 'Subject', 'content', 'Date']
-    return base_df.loc[:, cols].dropna()
+def escape_backslashes(s):
+    return re.sub(r'\\', r'\\\\', s)
 
 
-def clean_threads(subject_df, subject_col='Subject'):
+def load_data(data="enron.csv", nrows=5000, skiprows=0):
+    email_df = pd.read_csv(data, nrows=nrows, skiprows=skiprows).set_index("id", drop=False)
+    email_df["to"] = email_df.to.apply(lambda s: s.strip().split(","))
+    email_df["subject"] = email_df.subject.fillna("").apply(lambda s: escape_backslashes(s.strip()))
+    email_df["body"] = email_df.body.fillna("").apply(lambda s: escape_backslashes(s.strip()))
+    return email_df
+
+
+def clean_threads(subject_df, subject_col='subject'):
     subject_df = subject_df.astype(str)
 
     for index, row in subject_df.iterrows():
@@ -48,33 +55,35 @@ def process_neuralcoref(df, nlp=NLP):
         neuralcoref.add_to_pipe(nlp)
 
     for idx, email in zip(df.index, nlp.pipe(df['content'].tolist(), batch_size=75)):
-        df.at[idx, 'neuralcoref_content'] = email._.coref_resolved
+        df.at[idx, 'neuralcoref_body'] = email._.coref_resolved
 
     return df
 
 
 def process_emails(df, nlp=NLP, entity_list=ENTITIES_OF_INTEREST):
     new_cols = entity_list.copy()
-    new_cols.append('processed_content')
+    new_cols.append('processed_body')
     for col in new_cols:
         if col not in df:
             df[col] = ''
 
     for i, rows in df.iterrows():
-        df.at[i, 'processed_content'] = nlp(df.at[i, 'content'])
+        df.at[i, 'processed_body'] = nlp(df.at[i, 'body'])
         for entity in entity_list:
-            if entity in [ent.label_ for ent in list(df.at[i, 'processed_content'].ents)]:
-                df.at[i, entity] = [e.text for e in list(df.at[i, 'processed_content'].ents) if e.label_ == entity]
+            if entity in [ent.label_ for ent in list(df.at[i, 'processed_body'].ents)]:
+                df.at[i, entity] = [e.text for e in list(df.at[i, 'processed_body'].ents) if e.label_ == entity]
 
     return df
 
 
 def main(neuralcoref=False):
     start_time = time.time()
-    base_df = load_data(nrows=500)  # , skiprows=range(1,5000))
+    base_df = load_data(nrows=500)
     processed_df = parallelize_df(base_df, process_emails)
     if neuralcoref:
         parallelize_df(processed_df, process_neuralcoref, True, 'processed_emails.pkl')
+    else:
+        processed_df.to_pickle('processed_emails_nocoref.pkl')
     print('Time elapsed: {} min'.format((time.time() - start_time) / 60))
     return processed_df
 
