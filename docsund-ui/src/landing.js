838ce3b9d771afaae5dbf7f268b22e995b33ef7c
Andrew Carlson <acarl005@g.ucla.edu>
2019-07-28 23:16:59 -0400

    wip

--- docsund-ui/src/landing.js
+++ docsund-ui/src/landing.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React, { Component, useState } from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import { Layout, Tabs, Menu, Button } from "antd"
@@ -8,31 +8,34 @@ const navMenuPaths = [
   {
     path: "/",
     display: "Home",
-    component: () => "kahn tent"
+    component: () => <div>
+      fuck,
+    </div>
   },
   {
     path: "/enron",
     display: "Enron Emails",
-    component: () => <iframe src="http://enron.docsund.info" frameborder="0" width="100%" height="100%"></iframe>
+    component: () => <iframe src="http://enron.docsund.info" frameBorder="0" width="100%" height="100%"></iframe>
   },
   {
     path: "/sony",
     display: "Sony Emails",
-    component: () => <iframe src="http://sony.docsund.info" frameborder="0" width="100%" height="100%"></iframe>
+    component: () => <iframe src="http://sony.docsund.info" frameBorder="0" width="100%" height="100%"></iframe>
   },
   {
     path: "/clinton",
     display: "Clinton Emails",
-    component: () => <iframe src="http://clinton.docsund.info" frameborder="0" width="100%" height="100%"></iframe>
+    component: () => <iframe src="http://clinton.docsund.info" frameBorder="0" width="100%" height="100%"></iframe>
   },
   {
     path: "/dnc",
     display: "DNC Emails",
-    component: () => <iframe src="http://dnc.docsund.info" frameborder="0" width="100%" height="100%"></iframe>
+    component: () => <iframe src="http://dnc.docsund.info" frameBorder="0" width="100%" height="100%"></iframe>
   }
 ]
 
 function Main() {
+  const [active, setActive] = useState("0")
   return (
     <Router>
       <Layout>
@@ -47,15 +50,17 @@ function Main() {
           <Menu
             theme="dark"
             mode="horizontal"
-            selectedKeys={[navMenuPaths.findIndex(item => item.path === window.location.pathname)]}
+            selectedKeys={[active]}
             style={{ lineHeight: '64px', marginBottom: '64px' }}
             id="main-menu"
           >
-            {navMenuPaths.map((item, i) => <Menu.Item key={i}><Link to={item.path}>{item.display}</Link></Menu.Item>)}
+            {navMenuPaths.map((item, i) => <Menu.Item key={i}>
+              <Link to={item.path} onClick={() => setActive(i.toString())} key={i}>{item.display}</Link>
+            </Menu.Item>)}
           </Menu>
         </Header>
         <Content>
-          {navMenuPaths.map(item => <Route path={item.path} exact component={item.component} />)}
+          {navMenuPaths.map(item => <Route path={item.path} exact component={item.component} key={item.path} />)}
         </Content>
       </Layout>
     </Router>
