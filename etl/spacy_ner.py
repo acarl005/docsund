5818d31c9f345acc98fc9a57c4da0455a33d2e79
Danielle ONeil <danielle.oneil@berkeley.edu>
2019-08-03 19:02:08 -0400

    attempt at incorporating into real ETL

--- etl/spacy_ner.py
+++ etl/spacy_ner.py
@@ -10,11 +10,100 @@ import pandas as pd
 
 from to_neo4j import spacy_to_neo4j_etl
 
-csv.field_size_limit(sys.maxsize)
+import numpy as np
+import nltk
+from sklearn.base import BaseEstimator, TransformerMixin
+from scipy.sparse import csr_matrix
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from collections import Counter
+from joblib import load
+
+#csv.field_size_limit(sys.maxsize)
 ENTITIES_OF_INTEREST = ["PERSON", "NORP", "FAC", "ORG", "GPE", "LOC", "DATE", "TIME", "MONEY", "QUANTITY"]
 DESIRED_COLUMNS = ["body", "date", "from", "id", "subject", "to"]
 spacy_nlp = spacy.load("en_core_web_sm", disable=["parser", "textcat"])
 error = None
+
+LOG_CLF = load('spam_detector.joblib')
+
+class EmailToWords(BaseEstimator, TransformerMixin):
+    def __init__(self, stripHeaders=True, lowercaseConversion=True, punctuationRemoval=True,
+                 urlReplacement=True, numberReplacement=True, stemming=True):
+        self.stripHeaders = stripHeaders
+        self.lowercaseConversion = lowercaseConversion
+        self.punctuationRemoval = punctuationRemoval
+        self.urlReplacement = urlReplacement
+        self.numberReplacement = numberReplacement
+        self.stemming = stemming
+        self.stemmer = nltk.PorterStemmer()
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        X_to_words = []
+        for text in X:
+            if text is None:
+                text = 'empty'
+            if self.lowercaseConversion:
+                text = text.lower()
+
+            if self.punctuationRemoval:
+                text = text.replace('.', '')
+                text = text.replace(',', '')
+                text = text.replace('!', '')
+                text = text.replace('?', '')
+
+            word_counts = Counter(text.split())
+            if self.stemming:
+                stemmed_word_count = Counter()
+                for word, count in word_counts.items():
+                    stemmed_word = self.stemmer.stem(word)
+                    stemmed_word_count[stemmed_word] += count
+                word_counts = stemmed_word_count
+            X_to_words.append(word_counts)
+        return np.array(X_to_words)
+
+
+class WordCountToVector(BaseEstimator, TransformerMixin):
+    def __init__(self, vocabulary_size=1000):
+        self.vocabulary_size = vocabulary_size
+
+    def fit(self, X, y=None):
+        total_word_count = Counter()
+        for word_count in X:
+            for word, count in word_count.items():
+                total_word_count[word] += min(count, 10)
+        self.most_common = total_word_count.most_common()[:self.vocabulary_size]
+        self.vocabulary_ = {word: index + 1 for index, (word, count) in enumerate(self.most_common)}
+        return self
+
+    def transform(self, X, y=None):
+        rows = []
+        cols = []
+        data = []
+        for row, word_count in enumerate(X):
+            for word, count in word_count.items():
+                rows.append(row)
+                cols.append(self.vocabulary_.get(word, 0))
+                data.append(count)
+        return csr_matrix((data, (rows, cols)), shape=(len(X), self.vocabulary_size + 1))
+
+def apply_model(df, email_pipeline, filter_spam=True, log_clf=LOG_CLF):
+    X_ = np.array(df.body)
+    X_augmented_ = email_pipeline.fit_transform(X_)
+    y_pred_ = log_clf.predict(X_augmented_)
+
+    final_df = pd.concat([df.reset_index(drop=True),
+                          pd.Series(y_pred_, name='prediction')],
+                         axis=1, sort=False)
+
+    if filter_spam:
+        final_df = final_df.loc[final_df['prediction'] == 'ham']
+        final_df = final_df.drop(columns=['prediction'])
+
+    return final_df
 
 
 def clean_enron_list(entity_list):
@@ -64,6 +153,11 @@ def main(csv_path, nrows, progress):
     children = []
     combined_results = []
 
+    email_pipeline = Pipeline([
+        ("Email to Words", EmailToWords()),
+        ("Wordcount to Vector", WordCountToVector()),
+    ])
+
     try:
         with open(csv_path) as f:
             csv_reader = csv.reader(f)
@@ -102,6 +196,7 @@ def main(csv_path, nrows, progress):
         raise error
     processed_df = pd.DataFrame(combined_results, columns=csv_columns + ENTITIES_OF_INTEREST)
     del combined_results
+    processed_df = apply_model(processed_df, email_pipeline)
 
     for entity in ["PERSON", "ORG", "FAC"]:
         processed_df[entity] = processed_df[entity].apply(lambda s: clean_enron_list(s))
