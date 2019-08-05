d5fc80e00f437c75b4ac9516ff0a7ccdf239a175
Andrew Carlson <acarl005@g.ucla.edu>
2019-08-05 11:01:39 -0700

    lint the topic explorer code

--- docsund-ui/src/components/TopicExplorer/TopicExplorer.jsx
+++ docsund-ui/src/components/TopicExplorer/TopicExplorer.jsx
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
-import { Row, Col, Button, Typography, Slider, Alert, Form, Spin } from 'antd';
-import { topicExplorer } from './topic-explorer';
-import { fetchJSON } from '../../utils'
+import React, { Component } from "react"
+import {
+  Row, Col, Button, Slider, Alert, Spin
+} from "antd"
+import topicExplorer from "./topic-explorer"
+import { fetchJSON } from "../../utils"
+import appStore from "../../stores/AppStore"
 
-const { Text } = Typography;
 
 export default class TopicExplorer extends Component {
-
   // we hard-code the minimum number of topics, but we'll fetch the maximum from the API later...
   static minTopicNum = 3
 
   constructor(props) {
-    super(props);
+    super(props)
     if (TopicExplorer.maxTopicNum === undefined) {
       try {
-        TopicExplorer.maxTopicNum = topicExplorer.getMaxNumTopics();
+        TopicExplorer.maxTopicNum = topicExplorer.getMaxNumTopics()
         this.state = {
           loading: false,
           numTopics: topicExplorer.getOptimalNumTopics(),
@@ -26,11 +27,11 @@ export default class TopicExplorer extends Component {
       }
     }
 
-    this.onDisplayTopicDocuments = this.onDisplayTopicDocuments.bind(this);
+    this.onDisplayTopicDocuments = this.onDisplayTopicDocuments.bind(this)
   }
 
   componentDidMount() {
-    topicExplorer.createWordCloud();
+    topicExplorer.createWordCloud()
     this.renderTopicViz()
   }
 
@@ -50,20 +51,20 @@ export default class TopicExplorer extends Component {
 
   async renderTopicViz() {
     this.setState({ loading: true })
-    var topicData = await topicExplorer.getTopicsData(this.state.numTopics);
+    const topicData = await topicExplorer.getTopicsData(this.state.numTopics)
 
-    topicExplorer.plotTopicData(topicData, this.onTopicSelect.bind(this));
-    topicExplorer.plotWordCloud();
+    topicExplorer.plotTopicData(topicData, this.onTopicSelect.bind(this))
+    topicExplorer.plotWordCloud()
     this.setState({ loading: false })
   }
 
-  async onDisplayTopicDocuments () {
+  async onDisplayTopicDocuments() {
     const data = await fetchJSON(`${TOPIC_API_URL}/TM/topics/${this.state.selectedTopic}/documents?numTopics=${this.state.numTopics}`)
 
     const { docIDs } = data
-    appStore.toggleModal('topicSample');
-    appStore.setEmailModalView('list')
-    await appStore.fetchEmailsFromIDs(docIDs, 250);
+    appStore.toggleModal("topicSample")
+    appStore.setEmailModalView("list")
+    await appStore.fetchEmailsFromIDs(docIDs, 250)
   }
 
   render() {
@@ -75,10 +76,10 @@ export default class TopicExplorer extends Component {
         <Spin spinning={this.state.loading} tip="loading..." size="large">
           <Row>
             <Col span={12} style={{ padding: "15px" }}>
-              <svg id="word-cloud" viewBox={"0 0 500 500"} preserveAspectRatio={"xMidYMid meet"} width={"100%"}></svg>
+              <svg id="word-cloud" viewBox="0 0 500 500" preserveAspectRatio="xMidYMid meet" width="100%" />
             </Col>
             <Col span={12} style={{ padding: "15px" }}>
-              <svg id="topic-plot" viewBox={"0 0 500 500"} preserveAspectRatio={"xMidYMid meet"} width={"100%"}></svg>
+              <svg id="topic-plot" viewBox="0 0 500 500" preserveAspectRatio="xMidYMid meet" width="100%" />
             </Col>
           </Row>
         </Spin>
@@ -96,16 +97,16 @@ export default class TopicExplorer extends Component {
               onAfterChange={this.onTopicNumberChange.bind(this)}
               min={TopicExplorer.minTopicNum}
               max={TopicExplorer.maxTopicNum}
-              getTooltipPopupContainer={() => document.getElementById('topic-explorer-tab')}
+              getTooltipPopupContainer={() => document.getElementById("topic-explorer-tab")}
             />
           </div>
         </div>
         <Row>
           <Col span={3}>
             <Button type="primary" onClick={this.onDisplayTopicDocuments} disabled={this.state.selectedTopic === null}>
-              {this.state.selectedTopic !== null ?
-                `Get emails from topic ${this.state.selectedTopic} (of ${this.state.numTopics})` :
-                "Select a topic"
+              {this.state.selectedTopic !== null
+                ? `Get emails from topic ${this.state.selectedTopic} (of ${this.state.numTopics})`
+                : "Select a topic"
               }
             </Button>
           </Col>
--- docsund-ui/src/components/TopicExplorer/topic-explorer.js
+++ docsund-ui/src/components/TopicExplorer/topic-explorer.js
@@ -1,206 +1,196 @@
-import * as d3 from 'd3v5';
-import { fetchJSON } from "../../utils"
+import * as d3 from "d3v5"
 import { readFileSync } from "fs"
+import { fetchJSON } from "../../utils"
+
 const defaultWordCloud = readFileSync("./src/components/TopicExplorer/default-word-cloud.png.txt", "utf8")
 
 
-var plotWordCloud_ = function (wordCloudBytes) {
+function plotWordCloud_(wordCloudBytes) {
+  let imageByteText
+  if (wordCloudBytes === undefined) {
+    imageByteText = defaultWordCloud
+  } else {
+    imageByteText = wordCloudBytes
+  }
+  const wordCloudSvg = d3.select("#word-cloud")
+  wordCloudSvg.select("image").attr("xlink:href", `data:image/png;base64,${imageByteText}`)
+}
 
+export default {
+  plotTopicData(newTopicData, onSelectTopic) {
+    const topicPlotSvg = d3.select("svg#topic-plot")
+    const width = 500
+    const height = 500
+
+    const margin = {
+      left: 50,
+      right: 50,
+      top: 50,
+      bottom: 50
+    }
+
+    // Scaling functions
+    const calcRadius = function (area) {
+      return Math.sqrt(area / Math.PI)
+    }
+
+    const xScale = d3.scaleLinear()
+      .domain(d3.extent(newTopicData, d => d.x))
+      .range([margin.left, width - margin.right])
+
+    const yScale = d3.scaleLinear()
+      .domain(d3.extent(newTopicData, d => d.y))
+      .range([height - margin.bottom, margin.top])
+
+    const radScale = d3.scaleLinear()
+      .domain(d3.extent(newTopicData, d => calcRadius(d.size)))
+      .range([10, 40])
+
+    // Calculate the radius
+    newTopicData = newTopicData.map(d => ({
+      radius: radScale(calcRadius(d.size)),
+      size: d.size,
+      x: xScale(d.x),
+      y: yScale(d.y),
+      wordcloud: d.wordcloud,
+      topic: d.topic
+    }))
+
+    // Remove pre-existing circles from a potential previous plot
+    topicPlotSvg.selectAll("circle").remove()
+    topicPlotSvg.selectAll("text").remove()
+
+    // Draw the topic circles
+    const standardStrokeColor = "#10885C"
+    const hoveredBorderColor = "#4995D6"
+    const standardFillColor = "#20C78A"
+    const selectedFillColor = "#03afff"
+    const transitionDuration = 100
+    const standardStrokeWidth = 1.5
+    const hoveredStrokeWidth = 6
+    const expandedRadiusRatio = 1.4
+
+    // Plot the topic circles
+    const topicCircles = topicPlotSvg.selectAll("circle")
+      .data(newTopicData)
+      .enter()
+      .append("circle")
+      .attr("r", d => radScale(calcRadius(d.size)))
+      .attr("cx", d => d.x)
+      .attr("cy", d => d.y)
+      .style("stroke", standardStrokeColor)
+      .style("fill", standardFillColor)
+      .style("opacity", 0.8)
+      .style("stroke-width", standardStrokeWidth)
+      .on("mouseover", function () {
+        // When hovering over a topic, expand the border and turn it blue
+        const boundingCircle = d3.select(this)
+        boundingCircle.transition()
+          .duration(transitionDuration)
+          .style("stroke-width", hoveredStrokeWidth)
+          .style("stroke", hoveredBorderColor)
+      })
+      .on("mouseout", function () {
+        // When no longer hovering over a topic, return the border to its original state
+        const boundingCircle = d3.select(this)
+        boundingCircle.transition()
+          .duration(transitionDuration)
+          .style("stroke-width", standardStrokeWidth)
+          .style("stroke", standardStrokeColor)
+      })
+      .on("click", function (d) {
+        // When a topic circle is clicked, increase its size and display its word cloud.
+        // Also, return the size of the other topics to their original size, in case a previous
+        // topic was clicked.
+        topicCircles.transition()
+          .duration(transitionDuration)
+          .attr("r", d => radScale(calcRadius(d.size)))
+          .style("fill", standardFillColor)
+        plotWordCloud_(d.wordcloud)
+        const boundingCircle = d3.select(this)
+        boundingCircle.transition()
+          .duration(transitionDuration)
+          .attr("r", d => expandedRadiusRatio * radScale(calcRadius(d.size)))
+          .style("fill", selectedFillColor)
+
+        onSelectTopic(d.index)
+      })
+
+    // Write in the topic circle labels
+    const topicLabels = topicPlotSvg.selectAll("text")
+      .data(newTopicData)
+      .enter()
+      .append("text")
+    // Add your code below this line
+      .text(d => d.topic)
+      .attr("text-anchor", "middle")
+      .attr("font-family", "Light Sans Regular")
+      .attr("font-size", "10px")
+      .attr("x", d => d.x)
+      .attr("y", d => d.y)
+
+    // Collision force to prevent topic circle overlap
+    const ticked = function () {
+      topicCircles.attr("cx", d => d.x)
+        .attr("cy", d => d.y)
+      topicLabels.attr("x", d => d.x)
+        .attr("y", d => d.y)
+    }
+
+    d3.forceSimulation(newTopicData)
+      .force("collide", d3.forceCollide()
+        .strength(0.2)
+        .radius(d => d.radius + 2))
+      .on("tick", ticked)
+  },
+  createWordCloud() {
+    const width = 500
+    const height = 500
+
+    const wordCloudSvg = d3.select("svg#word-cloud")
+    const defaultImageByteText = defaultWordCloud
+
+    wordCloudSvg.selectAll("image")
+      .data([0])
+      .enter()
+      .append("svg:image")
+      .attr("xlink:href", `data:image/png;base64,${defaultImageByteText}`)
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", width)
+      .attr("height", height)
+  },
+
+  plotWordCloud(wordCloudBytes) {
+    let imageByteText
     if (wordCloudBytes === undefined) {
-        var imageByteText = defaultWordCloud;
+      imageByteText = defaultWordCloud
     } else {
-        var imageByteText = wordCloudBytes;
+      imageByteText = wordCloudBytes
     }
-    var wordCloudSvg = d3.select("#word-cloud");
-    var wordCloudImg = wordCloudSvg.select("image")
-        .attr("xlink:href", "data:image/png;base64," + imageByteText);
-};
+    const wordCloudSvg = d3.select("svg#word-cloud")
+    wordCloudSvg.select("image").attr("xlink:href", `data:image/png;base64,${imageByteText}`)
+  },
 
-export const topicExplorer = {
-    plotTopicData: function (newTopicData, onSelectTopic) {
+  getTopicsData(numTopics) {
+    return fetchJSON(`${TOPIC_API_URL}/TM/topicdistributiondata?numTopics=${numTopics}`)
+  },
 
-        var topicPlotSvg = d3.select("svg#topic-plot");
-        var width = 500,
-            height = 500;
+  getMaxNumTopics() {
+    const maxNumTopicsUrl = `${TOPIC_API_URL}/TM/nummodels`
+    const xhttp = new XMLHttpRequest()
+    xhttp.open("GET", maxNumTopicsUrl, false)
+    xhttp.send()
+    const maxNumTopics = parseInt(xhttp.responseText, 10)
+    return maxNumTopics
+  },
 
-        var margin = {
-            left: 50,
-            right: 50,
-            top: 50,
-            bottom: 50
-        };
-
-        // Scaling functions
-        var calcRadius = function (area) {
-            return Math.sqrt(area / Math.PI);
-        };
-
-        var xScale = d3.scaleLinear()
-            .domain(d3.extent(newTopicData, d => d.x))
-            .range([margin.left, width - margin.right]);
-
-        var yScale = d3.scaleLinear()
-            .domain(d3.extent(newTopicData, d => d.y))
-            .range([height - margin.bottom, margin.top]);
-
-        var radScale = d3.scaleLinear()
-            .domain(d3.extent(newTopicData, d => calcRadius(d.size)))
-            .range([10, 40]);
-
-        // Calculate the radius
-        newTopicData = newTopicData.map(function (d) {
-            return {
-                radius: radScale(calcRadius(d.size)),
-                size: d.size,
-                x: xScale(d.x),
-                y: yScale(d.y),
-                wordcloud: d.wordcloud,
-                topic: d.topic
-            };
-        });
-
-        // Remove pre-existing circles from a potential previous plot
-        topicPlotSvg.selectAll("circle").remove();
-        topicPlotSvg.selectAll("text").remove();
-
-        // Draw the topic circles
-        var standardStrokeColor = "#10885C";
-        var hoveredBorderColor = "#4995D6";
-        var standardFillColor = "#20C78A";
-        var selectedFillColor = "#03afff";
-        var transitionDuration = 100;
-        var standardStrokeWidth = 1.5;
-        var hoveredStrokeWidth = 6;
-        var expandedRadiusRatio = 1.4;
-
-        // Plot the topic circles
-        var topicCircles = topicPlotSvg.selectAll("circle")
-            .data(newTopicData)
-            .enter()
-            .append("circle")
-            .attr("r", d => radScale(calcRadius(d.size)))
-            .attr("cx", d => d.x)
-            .attr("cy", d => d.y)
-            .style("stroke", standardStrokeColor)
-            .style("fill", standardFillColor)
-            .style("opacity", 0.8)
-            .style("stroke-width", standardStrokeWidth)
-            .on("mouseover", function (d) {
-                // When hovering over a topic, expand the border and turn it blue
-                var boundingCircle = d3.select(this);
-                boundingCircle.transition()
-                    .duration(transitionDuration)
-                    .style("stroke-width", hoveredStrokeWidth)
-                    .style("stroke", hoveredBorderColor);
-            })
-            .on("mouseout", function (d) {
-                // When no longer hovering over a topic, return the border to its original state
-                var boundingCircle = d3.select(this);
-                boundingCircle.transition()
-                    .duration(transitionDuration)
-                    .style("stroke-width", standardStrokeWidth)
-                    .style("stroke", standardStrokeColor);
-            })
-            .on("click", function (d) {
-                // When a topic circle is clicked, increase its size and display its word cloud.
-                // Also, return the size of the other topics to their original size, in case a previous
-                // topic was clicked.
-                topicCircles.transition()
-                    .duration(transitionDuration)
-                    .attr("r", d => radScale(calcRadius(d.size)))
-                    .style("fill", standardFillColor);
-                plotWordCloud_(d.wordcloud);
-                var boundingCircle = d3.select(this);
-                boundingCircle.transition()
-                    .duration(transitionDuration)
-                    .attr("r", d => expandedRadiusRatio * radScale(calcRadius(d.size)))
-                    .style("fill", selectedFillColor);
-
-                onSelectTopic(d.index)
-            });
-
-        // Write in the topic circle labels
-        var topicLabels = topicPlotSvg.selectAll("text")
-            .data(newTopicData)
-            .enter()
-            .append("text")
-            // Add your code below this line
-            .text(d => d.topic)
-            .attr("text-anchor", "middle")
-            .attr("font-family", "Light Sans Regular")
-            .attr("font-size", "10px")
-            .attr("x", d => d.x)
-            .attr("y",  d => d.y);
-
-        // Collision force to prevent topic circle overlap
-        var ticked = function () {
-            topicCircles.attr("cx", d => d.x)
-                .attr("cy", d => d.y);
-            topicLabels.attr("x", d => d.x)
-                .attr("y", d => d.y);
-        };
-
-        var simulation = d3.forceSimulation(newTopicData)
-            .force("collide", d3.forceCollide()
-                .strength(0.2)
-                .radius(d => d.radius + 2))
-            .on("tick", ticked);
-    },
-    createWordCloud: function () {
-        var width = 500,
-            height = 500;
-
-        var margin = {
-            left: 50,
-            right: 50,
-            top: 50,
-            bottom: 50
-        };
-
-        var wordCloudSvg = d3.select("svg#word-cloud");
-        var defaultImageByteText = defaultWordCloud;
-
-        var wordCloudImg = wordCloudSvg.selectAll("image")
-            .data([0])
-            .enter()
-            .append("svg:image")
-            .attr("xlink:href", "data:image/png;base64," + defaultImageByteText)
-            .attr("x", 0)
-            .attr("y", 0)
-            .attr("width", width)
-            .attr("height", height);
-    },
-
-    plotWordCloud: function (wordCloudBytes) {
-
-        if (wordCloudBytes === undefined) {
-            var imageByteText = defaultWordCloud;
-        } else {
-            var imageByteText = wordCloudBytes;
-        }
-        var wordCloudSvg = d3.select("svg#word-cloud");
-        var wordCloudImg = wordCloudSvg.select("image")
-            .attr("xlink:href", "data:image/png;base64," + imageByteText);
-    },
-    getTopicsData (numTopics) {
-        return fetchJSON(`${TOPIC_API_URL}/TM/topicdistributiondata?numTopics=${numTopics}`)
-    },
-
-    getMaxNumTopics() {
-        var maxNumTopicsUrl = TOPIC_API_URL + "/TM/nummodels";
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", maxNumTopicsUrl, false);
-        xhttp.send();
-        var maxNumTopics = parseInt(xhttp.responseText, 10);
-        return maxNumTopics;
-    },
-
-    getOptimalNumTopics() {
-        const optimalNumTopicsUrl = TOPIC_API_URL + "/TM/topics"
-        const xhttp = new XMLHttpRequest()
-        xhttp.open("GET", optimalNumTopicsUrl, false)
-        xhttp.send()
-        const resp = JSON.parse(xhttp.responseText)
-        return resp.numberOfTopics
-    }
-};
+  getOptimalNumTopics() {
+    const optimalNumTopicsUrl = `${TOPIC_API_URL}/TM/topics`
+    const xhttp = new XMLHttpRequest()
+    xhttp.open("GET", optimalNumTopicsUrl, false)
+    xhttp.send()
+    const resp = JSON.parse(xhttp.responseText)
+    return resp.numberOfTopics
+  }
+}
